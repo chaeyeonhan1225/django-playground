4e254e5e47a0b5498d509340b23762213872cdb0
chaeyeon <gkscodus11@gmail.com>
2024-04-09 23:58:59 +0900

    feat: PostFactory 추가

--- post_mixin_and_factory/post/models.py
+++ post_mixin_and_factory/post/models.py
@@ -3,16 +3,30 @@ from django.db import models
 post_states = (("PUBLISHED", "발행"), ("HIDDEN", "숨김"), ("DELETED", "삭제"))
 
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=40, blank=False, null=False)
-    content = models.TextField(blank=False, null=False)
-    status = models.CharField(
-        max_length=20, choices=post_states, blank=False, null=False
-    )
+class TimeRecordingMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
+        abstract = True
+
+
+class Post(TimeRecordingMixin, models.Model):
+    title = models.CharField(max_length=40, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+    status = models.CharField(
+        max_length=20, choices=post_states, blank=False, null=False,
+        default="PUBLISHED"
+    )
+
+    class Meta:
         db_table = "post"
         ordering = ["-created_at"]
+
+
+class Comment(TimeRecordingMixin, models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.CharField(max_length=120, blank=False, null=False)
+
+    class Meta:
+        db_table = "comment"
---
+++ post_mixin_and_factory/post/post_factory.py
@@ -0,0 +1,12 @@
+from factory.django import DjangoModelFactory
+from factory import Faker
+
+from post_command.post.models import Post
+
+
+class PostFactory(DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = Faker("text", max_nb_chars=100)
+    content = Faker("text", max_nb_chars=200)
